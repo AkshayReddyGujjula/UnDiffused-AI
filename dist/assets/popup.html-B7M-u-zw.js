69c455888d51138d223f36dacc98c9c44a94f3a7
AkshayReddyGujjula <akshayreddyg07@gmail.com>
2026-02-10 11:16:48 +0000

    Added a Reverse Image search feature

--- dist/assets/popup.html-B7M-u-zw.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";import"./index-BFytIqUk.js";import"./ResultView-KbLL5_eE.js";
---
+++ dist/assets/popup.html-CR_qnwFy.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";import"./index-BEsnxUve.js";import"./ResultView-BfnlvwXG.js";
